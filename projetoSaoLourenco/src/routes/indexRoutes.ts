6cadb6e2d5d2fc17dc4a9b3c29230231affdd557
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-03-22 22:20:26 -0300

    Update indexRoutes.ts

--- projetoSaoLourenco/src/routes/indexRoutes.ts
+++ projetoSaoLourenco/src/routes/indexRoutes.ts
@@ -3,6 +3,7 @@ import escolaRouter from './escolaRoutes';
 import turmaRouter from './turmaRoutes';
 import alunoRouter from './alunoRoutes';
 import gestorRouter from './gestorRoutes';
+import alunos2024Router from './alunos2024Routes'; 
 
 const indexRouter = express.Router();
 
@@ -10,5 +11,6 @@ indexRouter.use('/escola', escolaRouter);
 indexRouter.use('/turma', turmaRouter);
 indexRouter.use('/aluno', alunoRouter);
 indexRouter.use('/gestor', gestorRouter);
+indexRouter.use('/alunos2024', alunos2024Router); 
 
 export default indexRouter;
