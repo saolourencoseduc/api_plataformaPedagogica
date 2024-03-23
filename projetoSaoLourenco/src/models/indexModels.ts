ec04859ceb460eb7e9b04511d0a54069d3ee28cd
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-03-22 22:19:46 -0300

    Update indexModels.ts

--- projetoSaoLourenco/src/models/indexModels.ts
+++ projetoSaoLourenco/src/models/indexModels.ts
@@ -2,5 +2,7 @@ import AlunoModel from "./alunoModel";
 import EscolaModel from "./escolaModel";
 import GestorModel from "./gestorModel";
 import TurmaModel from "./turmaModel";
+import Aluno2024Model from "./alunos2024Model";
 
-export { AlunoModel, EscolaModel, GestorModel, TurmaModel };
+export { AlunoModel, EscolaModel, GestorModel, TurmaModel, Aluno2024Model };
+
