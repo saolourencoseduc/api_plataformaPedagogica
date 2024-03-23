616820401e023bab07fdc31007e4f6fed04af37d
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-03-22 22:19:10 -0300

    Update indexControllers.ts

--- projetoSaoLourenco/src/controllers/indexControllers.ts
+++ projetoSaoLourenco/src/controllers/indexControllers.ts
@@ -2,5 +2,6 @@ import AlunoController from "./alunoController";
 import EscolaController from "./escolaController";
 import GestorController from "./gestorController";
 import TurmaController from "./turmaController";
+import Aluno2024Controller from "./alunos2024Controllers";
 
-export { AlunoController, EscolaController, GestorController, TurmaController };
+export { AlunoController, EscolaController, GestorController, TurmaController, Aluno2024Controller };
