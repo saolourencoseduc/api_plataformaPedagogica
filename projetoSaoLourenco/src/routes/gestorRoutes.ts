02cb3dffddd8301f06c597f87dfaae2e7ec52c99
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-03-28 08:26:45 -0300

    Update gestorRoutes.ts

--- projetoSaoLourenco/src/routes/gestorRoutes.ts
+++ projetoSaoLourenco/src/routes/gestorRoutes.ts
@@ -1,31 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import GestorController from '../controllers/gestorController';
 
 const router = express.Router();
 
-// Get all gestores
-router.get('/', async (req: Request, res: Response) => {
-    await GestorController.getAllGestores(req, res);
-});
-
-// Get gestor by ID
-router.get('/:id', async (req: Request, res: Response) => {
-    await GestorController.getGestorById(req, res);
-});
-
-// Create new gestor
-router.post('/', async (req: Request, res: Response) => {
-    await GestorController.createGestor(req, res);
-});
-
-// Update gestor by ID
-router.put('/:id', async (req: Request, res: Response) => {
-    await GestorController.updateGestor(req, res);
-});
-
-// Delete gestor by ID
-router.delete('/:id', async (req: Request, res: Response) => {
-    await GestorController.deleteGestor(req, res);
-});
+router.get('/:id', GestorController.getGestorById);
+router.get('/cpf/:cpf', GestorController.getGestorByCpf);
+router.get('/nome/:nome', GestorController.getGestorByNome);
+router.get('/', GestorController.getAllGestores);
+router.post('/', GestorController.createGestor);
+router.put('/:id', GestorController.updateGestor);
+router.delete('/:id', GestorController.deleteGestor);
 
 export default router;
